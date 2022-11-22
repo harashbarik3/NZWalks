3e02c253eb092df55df78ed7a3696b3482249bf4
harashbarik3 <harashbarik3@gmail.com>
2022-11-22 22:51:35 +0530

    End of Section3 Added Region Controller With Automapper

---
+++ NZWalks/NZWalks.API/Repositories/ResionRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class ResionRepository : IResionRepository
+    {
+        private readonly NZWalksDbContext _nZWalksDbContext;
+
+        public ResionRepository(NZWalksDbContext nZWalksDbContext)
+        {
+           _nZWalksDbContext = nZWalksDbContext;
+        }
+        public async Task<IEnumerable<Region>> GetAllAsync()
+        {
+            return await _nZWalksDbContext.Regions.ToListAsync();
+        }
+    }
+}
